7f3b2bb5c13ab29798254ce9805965f3667128b5
AryanManhas <122689335+AryanManhas@users.noreply.github.com>
2025-02-21 07:08:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "aryanmanhas6767@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jammu"
+
+// accountId = 2 // not allowed
+
+accountEmail = "am@gmail.com"
+
+
+accountPassword = "212121"
+
+
+accountCity = "Jaipur"
+
+
+// console.log(accountId);
+
+console.table([accountEmail , accountId , accountPassword , accountCity])
+
+
