4f377f85db893da1d654aa69cefb462c3d32c027
AryanManhas <122689335+AryanManhas@users.noreply.github.com>
2025-02-28 13:26:03 +0000

    dataTypes Deep

--- 01_basics/05_dataTypesSummary.js
+++ 01_basics/05_dataTypesSummary.js
@@ -1,3 +1,38 @@
 // Primitive
 
-// 7 Types : String
+// 7 Types : String , Number , Boolean , null(khali,empty not zero) , undefined , Symbol , BigInt
+
+// const score = 100
+// console.log(typeof(score));
+
+// const scoreValue = 100.3
+// console.log(typeof(scoreValue));
+
+// const temp = null;
+// console.log(temp);
+
+
+// let userEmail;
+// console.log(typeof(userEmail));
+
+//  const id = Symbol('123')
+//  const anotherId = Symbol('123')
+//  console.log(id === anotherId);
+//  console.log(typeof(id));
+//  console.log(typeof(anotherId));
+ 
+ 
+
+// const bigNumber = 213333234252n
+// console.log(typeof(bigNumber))
+
+
+// Reference (Non Primitive)
+
+// Array , Objects , Functions
+
+const heros = ["shakitman" , "naagraj" , "doga"];
+let myObj = {
+    name : "aryan",
+    age : 22
+}
