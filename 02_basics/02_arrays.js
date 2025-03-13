85fd1e40e04b587bfafbdab2d0e9cd87ebc8e5dd
AryanManhas <122689335+AryanManhas@users.noreply.github.com>
2025-03-13 02:33:53 +0000

    arrayfunctions

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,28 @@
+const marvel_heros=["Thor" , "IronMan", "SpiderMan"]
+const dc_heros=["SuperMan","Flash", "Batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const allNewHeros = [...marvel_heros , ...dc_heros]
+// console.log(allNewHeros) //spreads the value 
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array); //converts the array into simple 1,2,3,4,5,6,7,6,7,4,5 one array
+
+// console.log(Array.isArray("Aryan"))
+// console.log(Array.from("Aryan"))
+// console.log(Array.from({name: "Aryan"})) //intersting we have to provide we need to make key from array or key we have to define it
+
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
+
