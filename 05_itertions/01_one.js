d9c688630398d10ee1b68ac51bb5a63f74cc9e4a
AryanManhas <122689335+AryanManhas@users.noreply.github.com>
2025-07-17 17:34:11 +0000

    itertions

---
+++ 05_itertions/01_one.js
@@ -0,0 +1,14 @@
+let myArray = ["flash" , "batman" , "superman"]
+
+let arr = 0
+while (arr < myArray.length)
+{
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 1
+
+do {
+    
+} while (condition);
